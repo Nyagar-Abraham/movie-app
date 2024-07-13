ba7455138e93a16a874dcfe329be09c4126cc1d6
Nyagar Abraham <abrahamowino@gmail.com>
2024-07-13 20:38:53 +0300

    final

--- app/(auth)/sign-up/[[...sign-up]]/page.tsx
+++ app/(auth)/sign-up/[[...sign-up]]/page.tsx
@@ -4,14 +4,24 @@ export default function Page() {
 	return (
 		<div className="flex-center mt-10 ">
 			<SignUp
-				// appearance={{
-				// 	elements: {
-				// 		card: 'bg-dark100-light0 text-light100-dark0 shadow-md', // Custom CSS classes for the card
-				// 		headerTitle: 'text-lg font-semibold text-rose-950', // Custom CSS classes for the header title
-				// 		buttonPrimary:
-				// 			'bg-gradient-to-br from-rose-950 to-slate-pink-950 text-rose-100', // Custom CSS classes for the primary button
-				// 	},
-				// }}
+				appearance={{
+					elements: {
+						formButtonPrimary:
+							'bg-gradient-to-br from-red to-red/70 text-pure-white  border-none',
+						footerActionLink: 'text-red ',
+						socialButtonsBlockButton:
+							'text-slate-50 bg-gray-700 hover:bg-gray-600',
+					},
+					variables: {
+						colorPrimary: '#ff6b6b', // Red color in hex format
+						colorTextOnPrimaryBackground: '#ffffff', // White color in hex format
+						colorBackground: '#212529', // Dark gray color in hex format
+						colorInputBackground: '#495057',
+						colorText: '#f1f3f5',
+						colorNeutral: '#ced4da',
+						colorTextSecondary: '#e9ecef',
+					},
+				}}
 			/>
 		</div>
 	);
